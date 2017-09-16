bd7d6f7fbb4fadb40dea8fb503459557100f9216
Ganeshkumar Ramachandran <gramachandran@Ganeshkumars-MacBook-Pro.local>
2017-09-17 02:30:53 +0530

    store the indexed result in memory for web server

--- parser/index.js
+++ parser/index.js
@@ -2,13 +2,17 @@
  * Created by gramcha on 15/09/17.
  */
 let lineByLine = require('n-readlines');
+let MultiHashMap = require('multi-hashmap').MultiHashMap;
+let indexCache = new MultiHashMap('key', 'tags');
+const defaultIndexFilePath = 'tags';
 
 function createIndex(fileList, indexfilepath) {
+    if (!indexfilepath) {
+        indexfilepath = defaultIndexFilePath;
+    }
     console.log('indexing....');
     let fs = require('fs')
-    let indexFile = fs.createWriteStream(indexfilepath/*, {
-     flags: 'a' // 'a' means appending (old data will be preserved)
-     }*/);
+    let indexFile = fs.createWriteStream(indexfilepath);
     fileList.forEach((filename, index) => {
         // console.log("count ", index, " fname ", filename);
         let liner = new lineByLine(filename);
@@ -22,8 +26,14 @@ function createIndex(fileList, indexfilepath) {
             //let match
             let newString = str.replace(/[^auto\s*|const\s*|unsigned\s*|signed\s*|register\s*|volatile\s*|static\s*][a-zA-Z0-9_-]* ([\*a-zA-Z0-9_-]{1,})/g, function (match, captureOne) {
                 // console.log(captureOne);
-                const indexLine = captureOne + '\t' + 'v' + '\t' + lineNumber + '\t' + filename +'\t'+offset +'\n';
+                const indexLine = captureOne + '\t' + 'v' + '\t' + lineNumber + '\t' + filename + '\t' + offset + '\n';
                 indexFile.write(indexLine);
+                indexCache.insert(captureOne, {
+                    'fname': filename,
+                    'type': 'v',
+                    'lineNo': lineNumber,
+                    'offset': offset
+                });
                 // console.log(indexLine);
                 // return captureOne;
             });
@@ -33,8 +43,14 @@ function createIndex(fileList, indexfilepath) {
                 // console.log('fname - ', captureone, ' match -', match);
                 let words = match.split(' ');
                 words = words[1].split('(');
-                const indexLine = words[0] + '\t' + 'f' + '\t' + lineNumber + '\t' + filename +'\t'+offset + '\n';
+                const indexLine = words[0] + '\t' + 'f' + '\t' + lineNumber + '\t' + filename + '\t' + offset + '\n';
                 indexFile.write(indexLine);
+                indexCache.insert(words[0], {
+                    'fname': filename,
+                    'type': 'f',
+                    'lineNo': lineNumber,
+                    'offset': offset
+                });
                 // console.log(indexLine);
                 // return captureone;
             });
@@ -45,5 +61,5 @@ function createIndex(fileList, indexfilepath) {
     indexFile.end();
 }
 
-let parser = {createIndex};
+let parser = {createIndex, indexCache, defaultIndexFilePath};
 module.exports = parser;
