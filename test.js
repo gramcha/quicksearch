1a017cf72486d128b18dae51e5def1aa7464bfd3
Ganeshkumar Ramachandran <gramachandran@Ganeshkumars-MacBook-Pro.local>
2017-09-16 17:27:04 +0530

    temp update

--- test.js
+++ test.js
@@ -17,31 +17,33 @@ let t3 = new Date();
 //redisLrand48
 query.findCodeSnippet('tags','redisLrand48',true,(resultlist)=>{
     let t4 = new Date();
-    // console.log("resultlist ",resultlist);
+    console.log("resultlist ",resultlist);
     console.log("result count - ",resultlist.length);
     console.log((t4-t3)/1000);
 });
 
+
+
+//
 // var fs = require('fs');
 // let readline = require('readline');
 // let read = false;
 // //
-// let fp = fs.openSync('/Users/gramcha/source/trials/redis/00-RELEASENOTES','r');
-// // let fd = fs.createReadStream(null,{start: 129,fd:fp});
-// // console.log('===========');
-// //
-// // console.log(fd);
-// // console.log('===========');
-// readline.createInterface({
-//     input: fs.createReadStream(null,{fd:fp,start: 130}),
-//     terminal: false
-// }).on('line', function (line) {
-//     if(false === read && line.length >0)
-//     {
-//         console.log("length-",line.length);
-//         console.log(line);
-//         read = true;
-//     }
-// }).on('close', function () {
+// let fp = fs.openSync('/Users/gramachandran/source/trials/redis/00-RELEASENOTES', 'r');
+// let stream = fs.createReadStream(null, {fd: fp, start: 130});
 //
-// });
+// try{
+//     readline.createInterface({
+//         input: stream,
+//         terminal: false
+//     }).on('line', function (line) {
+//         if (false === read && line.length > 0) {
+//             console.log("length-", line.length);
+//             console.log(line);
+//             read = true;
+//         }
+//         console.log('******');
+//     });
+// }catch (err){
+//
+// }
